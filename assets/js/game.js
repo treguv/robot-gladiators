c449e43f34b7fa48403ccafe2fdb129c7534d8f9
treguv <vladislavtregubov00@gmail.com>
2020-09-28 14:13:34 -0700

    Added logic

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,43 @@
-window.alert("This is a javascript!");
+var playerName = window.prompt("What is your robots name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+//Enemy Stats
+var enemyName = "Roberto";
+var enemyHealth = 100;
+var enemyAttck = 10;
+console.log(playerName);
+
+fight(playerName);
+
+//This is how you make a function
+function fight(name){
+    window.alert("Welcome to robot gladiators");
+
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    enemyHealth -= playerAttack;
+    // Log a resulting message to the console so we know that it worked.
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health left");
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    playerHealth -= enemyAttck;
+    // Log a resulting message to the console so we know that it worked.
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health left");
+    // check enemy's health
+    if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+    } 
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+    // put new code under this
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+
+    // check player's health
+    if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+    } 
+    else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+}
