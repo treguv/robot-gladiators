5f1b582ca263e3c2c0776ce7d1677fa0cdcf4a3b
treguv <vladislavtregubov00@gmail.com>
2020-10-03 12:42:17 -0700

    Change shop to accept integer input

--- assets/js/game.js
+++ assets/js/game.js
@@ -65,20 +65,17 @@ function fight(enemy){
 function shop(){
     //console.log("Entered the shop");
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     );
     
-    switch(shopOptionPrompt){
-        case "REFILL":
-        case("refill"):
+    switch(parseInt(shopOptionPrompt)){
+        case 1:
             playerInfo.refillHealth();
             break;
-        case "UPGRADE":
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
-        case "LEAVE":
-        case "leave":
+        case 3:
             window.alert("Leaving the store.");
         
             // do nothing, so function will end
